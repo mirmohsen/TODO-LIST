d4f6b3a5a08e95b33905171f2a19188c974b148b
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-07 10:34:23 +0330

    change method get to post

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
-// cats.controller.ts
-import { Controller, Body, Get } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
 import { UserService } from './user.service';
+import { CryptoService } from '../common/classes/utils.class';
 
 @Controller('cats')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get()
+  @Post()
   create(@Body() userDto: UserDto) {
     this.userService.create(userDto);
   }

f481c3793ad0dd2086aa95f1fc6e782167715840
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-07 10:35:17 +0330

    create user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { User } from './user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: User }])],
   controllers: [UserController],
   providers: [UserService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
-// cats.service.ts
 import { Injectable } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  create(userDto: UserDto) {
-    console.log(userDto);
+  constructor(@InjectModel(User.name) private UserModel: Model<User>) {}
+
+  async create(userDto: UserDto): Promise<User> {
+    const createUser = new this.UserModel(userDto);
+    return createUser.save();
   }
 }

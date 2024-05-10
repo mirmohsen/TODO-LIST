b84b67f03af7f10441a7dc737d1b52e36087e9fe
mirmohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-11 00:48:42 +0330

    added entity to provider

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,13 +4,14 @@ import { UserService } from './user.service';
 import { User, UserSchema } from './user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CryptoService } from 'src/common/classes/utils.class';
+import { UserEntity } from './entity/user.entity';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UserController],
-  providers: [UserService, CryptoService],
+  providers: [UserService, CryptoService, UserEntity],
 })
 export class UserModule {
   constructor() {}

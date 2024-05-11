ee10dd5692acd2395916b066d14e492079c2e612
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-11 19:11:15 +0330

    create method get

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -13,15 +13,20 @@ export class UserService {
   ) {}
 
   private async findDuplicateEmail(email: string): Promise<boolean> {
-    return !!this.UserModel.findOne({ email });
+    return this.UserModel.findOne({ email });
   }
 
   public async create(userDto: UserDto): Promise<User> {
     if (await this.findDuplicateEmail(userDto.email))
       throw new ForbiddenException('email not uniqe');
+
     return this.UserModel.create({
       ...userDto,
       password: this.cryptoService.hashPassword(userDto.password),
     });
+  }
+
+  public async get(): Promise<any> {
+    return this.UserModel.find({}).select('_id username email');
   }
 }

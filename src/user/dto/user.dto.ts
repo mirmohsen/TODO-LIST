8d2b863dadff7a654494901a3e9d000e277769e7
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-11 16:48:16 +0330

    added decorator for checking validation in UserDto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class UserDto {
+  @IsNotEmpty()
+  @Length(4, 20)
   readonly username: string;
+  @Length(4, 20)
   readonly password: string;
+  @IsEmail()
   readonly email: string;
 }

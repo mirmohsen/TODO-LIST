af80cbe3c11217e806d631da4421d2b2aba76394
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-13 12:13:08 +0330

    create collection for use to get user

--- src/user/entity/user.entity.ts
+++ src/user/entity/user.entity.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { User, UserDocument } from '../user.schema';
 
 export class UserRo {
   id: string;
@@ -7,13 +8,21 @@ export class UserRo {
 }
 @Injectable()
 export class UserEntity {
-  public async response(user) {
+  public response(user): UserRo {
     const returnData: UserRo = {
       id: user.id,
       username: user.username,
       email: user.email,
     };
 
-    return await returnData;
+    return returnData;
+  }
+
+  public collection(users: UserDocument[]): UserRo[] {
+    const returnData: UserRo[] = [];
+    for (const user of users) {
+      returnData.push(this.response(user));
+    }
+    return returnData;
   }
 }

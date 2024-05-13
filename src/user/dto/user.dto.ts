8628b142142d18dc9378fbae421a59d12cb69fba
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-13 12:11:28 +0330

    create dto for update user and add decorator IsOptional

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,11 +1,37 @@
-import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+import {
+  IsEmail,
+  IsMongoId,
+  IsNotEmpty,
+  IsOptional,
+  Length,
+} from 'class-validator';
 
 export class UserDto {
   @IsNotEmpty()
   @Length(4, 20)
-  readonly username: string;
+  username: string;
+
   @Length(4, 20)
-  readonly password: string;
+  password: string;
+
   @IsEmail()
-  readonly email: string;
+  email: string;
+}
+
+export class UpdateUserRo {
+  @IsMongoId()
+  _id: string;
+
+  @IsNotEmpty()
+  @Length(4, 20)
+  @IsOptional()
+  username: string;
+
+  @Length(4, 20)
+  @IsOptional()
+  password: string;
+
+  @IsEmail()
+  @IsOptional()
+  email: string;
 }

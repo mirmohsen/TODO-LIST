d49af37d2faadaff3623bc792acd31c5b7baff91
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-14 13:13:22 +0330

    added rate limit to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-
 import { UserModule } from './user/user.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import 'dotenv/config';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGO_URL), UserModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    UserModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}

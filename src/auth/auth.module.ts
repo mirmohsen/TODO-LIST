e07546abfc144165227acd1706dc20e9807f2a79
mohsen <mirmohsenmirazimitarki78@gmail.com>
2024-05-18 16:19:00 +0330

    added jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,19 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { CryptoService } from 'src/common/classes/utils.class';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, CryptoService],
+  providers: [AuthService, CryptoService, JwtService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,42 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { CryptoService } from 'src/common/classes/utils.class';
 import { AuthDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
     private userService: UserService,
+    private jwtService: JwtService,
     private readonly cryptoService: CryptoService,
   ) {}
 
-  async signIn(payload: AuthDto): Promise<any> {
+  async signIn(payload: AuthDto): Promise<{ access_token: string }> {
     const user = await this.userService.findOneUser(payload);
 
-    if (
-      this.cryptoService.hashPassword(String(user.password?.password)) ===
-      this.cryptoService.hashPassword(String(payload.password))
-    ) {
-      throw new UnauthorizedException();
+    if (user) {
+      // console.log(
+      //   'equal pass ---->',
+      //   this.cryptoService.hashPassword(payload.password) !==
+      //     this.cryptoService.hashPassword(user.password),
+      // );
+      if (
+        this.cryptoService.hashPassword(payload.password) !==
+        this.cryptoService.hashPassword(user.password)
+      ) {
+        const payload = {};
+        return {
+          access_token: await this.jwtService.signAsync(payload),
+        };
+      } else {
+        throw new UnauthorizedException();
+      }
+    } else {
+      throw new NotFoundException();
     }
-    return 'logined';
-    // const { password, ...result } = user;
-
-    // return result;
   }
 }
